4efebbc5882f6e02e91f25b3bf0d8654d0e1c352
fikrifyanto <fikrifyanto@gmail.com>
2023-12-09 19:58:39 +0700

    fix nuxt config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "https://fikrifyanto.github.io/",
-  assetPrefix: "https://fikrifyanto.github.io/",
-  output: "export",
   distDir: "dist",
 }
 
