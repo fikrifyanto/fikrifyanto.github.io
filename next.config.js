0fe4f668acb035d743ef890a238db6a8942f0171
fikrifyanto <fikrifyanto@gmail.com>
2023-12-09 20:12:36 +0700

    fix github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "https://fikrifyanto.github.io/",
+  assetPrefix: "https://fikrifyanto.github.io/",
+  output: "export",
   distDir: "dist",
 }
 
